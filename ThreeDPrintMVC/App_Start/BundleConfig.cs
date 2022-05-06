d6ade0ae3bc66747b0deeb1f2815a505bf9a5cf7
Hayden Linville <hayden.linville@gmail.com>
2022-05-06 09:17:53 -0400

    updated bootstrap

--- ThreeDPrintMVC/App_Start/BundleConfig.cs
+++ ThreeDPrintMVC/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace ThreeDPrintMVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/poppers.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
